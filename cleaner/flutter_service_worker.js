cc3a2f1724b2ce06ad64de81c8ef4bdc564d0f39
Sina <dalvandsina@yahoo.com>
2020-03-17 13:20:37 +0330

    Copyright Added

--- cleaner/flutter_service_worker.js
+++ cleaner/flutter_service_worker.js
@@ -4,13 +4,13 @@ const RESOURCES = {
   "/assets\AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets\FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "8d45dcc7af3e9200895fab694f24cfc2",
+"/assets\LICENSE": "ce7ac68037d3a967527abaf9342fb440",
 "/assets\packages\cupertino_icons\assets\CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "c4a350491384a1a9920907bc64d8c039",
-"/main.dart.js": "fc331abaf5ea4839c466e80918a85c58",
+"/main.dart.js": "7507eb7bbe5a53f1ec518d0472ba7224",
 "/manifest.json": "9c4b368ace2f9e468343dc6e7730c213"
 };
 
