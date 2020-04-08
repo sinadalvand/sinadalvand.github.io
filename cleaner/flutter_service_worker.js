b0a67453f7703cafa048a617428f07de0429e3fd
Sina <dalvandsina@yahoo.com>
2020-04-09 03:36:36 +0430

    Column calculation Added

--- cleaner/flutter_service_worker.js
+++ cleaner/flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "c4a350491384a1a9920907bc64d8c039",
-"/main.dart.js": "7507eb7bbe5a53f1ec518d0472ba7224",
+"/main.dart.js": "d355569e995884b45bb47fce5b51c083",
 "/manifest.json": "9c4b368ace2f9e468343dc6e7730c213"
 };
 
